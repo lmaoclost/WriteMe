fcc9d68f97f19c9cf495e0f543e141f5f5c2577f
lmaoclost <renansmoliveira@gmail.com>
2023-01-27 19:07:57 -0300

    feat: Created story table

---
+++ prisma/migrations/20230127220726_created_story_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "stories" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "stories_pkey" PRIMARY KEY ("id")
+);
