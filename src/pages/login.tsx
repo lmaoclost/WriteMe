cfd6bfa1cfd7f11e4b0bc1e2cacf4b4f3c9deaa3
lmaoclost <renansmoliveira@gmail.com>
2023-02-16 14:22:18 -0300

    fix: adjusted login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,27 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 
 export default function Login() {
   const { data: session } = useSession();
 
-  console.log(session)
   if (session) {
     return (
       <>
         Signed in as {session.user?.email} <br />
-        <Image src={session.user?.image || ''} alt="" />
+        <Image
+          src={session.user?.image || ""}
+          width={150}
+          height={150}
+          alt=""
+        />
         <button onClick={() => signOut()}>Sign out</button>
       </>
-    )
+    );
   }
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn('google')}>Google</button>
+      <button onClick={() => signIn("google")}>Google</button>
     </>
-  )
+  );
 }
