f629e6878f8da3771f256b260292842671495bcd
lmaoclost <renansmoliveira@gmail.com>
2023-02-24 23:24:11 -0300

    fix: adjusted prisma auth

---
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -0,0 +1,20 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '@/lib/prismadb'
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+    }),
+  ],
+  pages: {
+    signIn: '/login',
+    error: '/login', // Error code passed in query string as ?error=
+  },
+}
+
+export default NextAuth(authOptions)
